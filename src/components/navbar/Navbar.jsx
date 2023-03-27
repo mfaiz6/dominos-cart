89f0ebf1e3a9d8ac3b7651af3d78fbfc03972a84
mfaiz6 <Mohammadfaiz_2019bcse002@nitsri.net>
2023-03-27 14:31:22 +0530

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './navbar.css'
+import logo from '../../assets/logo_white.svg'
+
+const Navbar = () => {
+    return (
+        <>
+            <div>
+                <img src={logo} alt="logo" />
+            </div>
+        </>
+    )
+}
+
+export default Navbar
